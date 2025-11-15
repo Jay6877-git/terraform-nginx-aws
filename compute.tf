c8560fc0d860bb957c0d9506fab6aac32fd8145f
Jay Patel <jaympatel6877@gmail.com>
2025-11-15 14:46:46 -0600

    apply auto fetch ubuntu ami

--- compute.tf
+++ compute.tf
@@ -1,5 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Owner is Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-*-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami                         = "ami-0d87339b5127ec34f"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
 
